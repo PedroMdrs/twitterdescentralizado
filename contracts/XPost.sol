0c9835f8ea7f3585b0b7054519a1b49a2d2cf743
Pedro Medeiros <contareservadopedro@Outlook.com>
2023-09-22 15:15:46 -0300

    xPost functions

--- contracts/XPost.sol
+++ contracts/XPost.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.19;
 import "hardhat/console.sol";
 
 contract XPost {
-  constructor() {
-    console.log("x post");
+  uint256 totalPosts;
+
+  constructor(){
+    console.log('post x');
+  }
+
+  function createPost() public {
+    totalPosts += 1;
+    console.log("%s criou o post", msg.sender);
+  }
+
+  function getTotalPosts() public view returns (uint256){
+    console.log("eu tenho %d posts", totalPosts);
+    return totalPosts;
   }
 }
